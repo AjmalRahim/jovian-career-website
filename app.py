6450dfeece36f45000633539a8f31dd8617012ca
AjmalRahim <149904848+AjmalRahim@users.noreply.github.com>
2023-12-13 08:09:59 +0000

    dynamic page for each page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
-from flask import Flask, render_template, request, redirect, url_for, flash
-from database import load_jobs_from_db
-from sqlalchemy.sql import text
+from flask import Flask, render_template, jsonify, request
+
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -10,7 +10,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_jovian():
     jobs = load_jobs_from_db()
-    return render_template("home.html", jobs=jobs, company_name='Jovian')
+    return render_template("home.html", jobs=jobs)
+
+@app.route('/job/<id>')
+def show_job(id):
+    job = load_job_from_db(id)
+    return render_template('jobpage.html', job=job)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, text
 import os
 
-db_connection_string = os.environ['DB_CONNECTION_STRING']
+db_connection_string =  "mysql+pymysql://admin:GAToSLGOVNUBu6gBziLA@jovian.c9ztkzyavckv.ap-south-1.rds.amazonaws.com/joviancareer"
 
 engine = create_engine(db_connection_string)
 
@@ -14,3 +14,12 @@ def load_jobs_from_db():
     for row in result.all():
         jobs.append(dict(row._asdict()))
     return jobs
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+     result = conn.execute(text("SELECT * FROM jobs WHERE id = :val").bindparams(val=id))
+     rows = result.all()
+     if len(rows) == 0:
+        return None
+     else:
+        return rows[0]
